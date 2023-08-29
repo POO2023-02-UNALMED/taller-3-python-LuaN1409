8df53890c9f61435036ac687138d866906f78c32
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-08-28 22:33:08 -0500

    Revert "Clases M, TV, y C"
    This reverts commit 9789d12465dfe8a28d2c45c19891ce48b9ed11a9.

--- Marca.py
+++
@@ -1,9 +0,0 @@
-class Marca:
-    def __init__(self, nombre):
-        self.nombre = nombre
-    
-    def getNombre(self):
-        return self.nombre
-    
-    def setNombre(self, nombre):
-        self.nombre = nombre
