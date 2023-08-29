22e834bde36abbda982b7b5b9350f27217546732
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-08-28 23:16:05 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -7,7 +7,7 @@ class TV:
         self. canal = 1
         self. precio = 500
         self.volumen = 1
-        self.control = None
+        self.control 
         TV.numTV += 1
         
     def setMarca(self, marca):
